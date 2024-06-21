5a97a3fb314b36258cd8899e4d929c13309b21a7
Jorge Zumba Morales <108309919+JorgeZumbaMorales@users.noreply.github.com>
2024-06-21 18:48:26 -0500

    Update sync.js

--- Proyecto_App/server/sync.js
+++ Proyecto_App/server/sync.js
@@ -11,16 +11,16 @@ const UsuarioMateriaPrima = require('./models/UsuarioMateriaPrima');
 const Ventas = require('./models/Ventas');
 
 // Definir relaciones
-Inconveniente.belongsTo(Produccion, { foreignKey: 'ID_PRODUCCION' });
-InventarioProductoTerminado.belongsTo(Produccion, { foreignKey: 'ID_PRODUCCION' });
-Produccion.belongsTo(InventarioMateriaPrima, { foreignKey: 'ID_MATERIA_PRIMA' });
-ProduccionEtapa.belongsTo(Produccion, { foreignKey: 'ID_PRODUCCION' });
-ProduccionEtapa.belongsTo(Etapa, { foreignKey: 'ID_ETAPA' });
-Usuario.belongsTo(Rol, { foreignKey: 'ID_ROL' });
-UsuarioMateriaPrima.belongsTo(Usuario, { foreignKey: 'ID_USUARIO' });
-UsuarioMateriaPrima.belongsTo(InventarioMateriaPrima, { foreignKey: 'ID_MATERIA_PRIMA' });
-Ventas.belongsTo(Usuario, { foreignKey: 'ID_USUARIO' });
-Ventas.belongsTo(InventarioProductoTerminado, { foreignKey: 'ID_PRODUCTO' });
+Inconveniente.belongsTo(Produccion, { foreignKey: 'id_produccion' });
+InventarioProductoTerminado.belongsTo(Produccion, { foreignKey: 'id_produccion' });
+Produccion.belongsTo(InventarioMateriaPrima, { foreignKey: 'id_materia_prima' });
+ProduccionEtapa.belongsTo(Produccion, { foreignKey: 'id_produccion' });
+ProduccionEtapa.belongsTo(Etapa, { foreignKey: 'id_etapa' });
+Usuario.belongsTo(Rol, { foreignKey: 'id_rol' });
+UsuarioMateriaPrima.belongsTo(Usuario, { foreignKey: 'id_usuario' });
+UsuarioMateriaPrima.belongsTo(InventarioMateriaPrima, { foreignKey: 'id_materia_prima' });
+Ventas.belongsTo(Usuario, { foreignKey: 'id_usuario' });
+Ventas.belongsTo(InventarioProductoTerminado, { foreignKey: 'id_producto' });
 
 // Sincronizar los modelos con la base de datos
 sequelize.sync({ force: true })
