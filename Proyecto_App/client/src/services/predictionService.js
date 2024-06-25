18016af5a9f7a8ff09486644b162165b7e428ea7
Hugofer2112 <hugofer211@gmail.com>
2024-06-24 22:20:23 -0500

    Update predictionService.js

--- Proyecto_App/client/src/services/predictionService.js
+++ Proyecto_App/client/src/services/predictionService.js
@@ -1,75 +1,38 @@
 import * as tf from '@tensorflow/tfjs';
-import axios from 'axios';
 
-// Obtener datos históricos
-async function obtenerDatosHistoricos() {
-  const response = await axios.get('http://localhost:3307/produccion');
-  return response.data;
-}
+export const predecirNecesidad = async (datosEntrenamiento) => {
+  // Preparar los datos
+  const inputs = datosEntrenamiento.map(d => [d.totalProduccion, d.totalMateriaPrimaUsada, d.materiaPrimaRestante, d.ultimoIngreso]);
+  const labels = datosEntrenamiento.map(d => d.ultimoIngreso);
 
-// Entrenar el modelo
-async function entrenarModelo() {
-  const datos = await obtenerDatosHistoricos();
-  console.log('Datos históricos:', datos);
-
-  // Filtrar datos inválidos
-  const datosFiltrados = datos.filter(d => {
-    const cantidadUso = parseFloat(d.cantidad_uso);
-    const cantidadProducida = parseFloat(d.cantidad_producida);
-    const materiaPrimaRestante = parseFloat(d.materia_prima_restante);
-    return !isNaN(cantidadUso) && !isNaN(cantidadProducida) && !isNaN(materiaPrimaRestante);
-  });
-
-  console.log('Datos filtrados:', datosFiltrados);
-
-  const inputs = datosFiltrados.map(d => [
-    parseFloat(d.cantidad_uso),
-    parseFloat(d.cantidad_producida),
-    parseFloat(d.materia_prima_restante)
-  ]);
-  const labels = datosFiltrados.map(d => parseFloat(d.cantidad_uso)); // Asumiendo que queremos predecir la cantidad de uso
-
-  console.log('Inputs:', inputs);
-  console.log('Labels:', labels);
-
-  const inputTensor = tf.tensor2d(inputs, [inputs.length, 3]);
+  const inputTensor = tf.tensor2d(inputs);
   const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
 
-  console.log('Input Tensor:', inputTensor.toString());
-  console.log('Label Tensor:', labelTensor.toString());
-
+  // Crear y entrenar el modelo
   const model = tf.sequential();
-  model.add(tf.layers.dense({ inputShape: [3], units: 16, activation: 'relu' }));
+  model.add(tf.layers.dense({ units: 8, activation: 'relu', inputShape: [4] }));
+  model.add(tf.layers.dense({ units: 4, activation: 'relu' }));
   model.add(tf.layers.dense({ units: 1 }));
 
   model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
 
-  await model.fit(inputTensor, labelTensor, { epochs: 50 });
+  await model.fit(inputTensor, labelTensor, {
+    epochs: 100,
+    batchSize: 1
+  });
 
-  console.log('Modelo entrenado:', model);
+  // Predecir el próximo ingreso
+  const [ultimaSemana] = datosEntrenamiento.slice(-1);
+  const inputParaPrediccion = tf.tensor2d([[ultimaSemana.totalProduccion, ultimaSemana.totalMateriaPrimaUsada, ultimaSemana.materiaPrimaRestante, ultimaSemana.ultimoIngreso]]);
 
-  return model;
-}
+  const prediccion = model.predict(inputParaPrediccion);
+  const resultado = await prediccion.data();
 
-// Predecir la necesidad de materia prima
-export async function predecirNecesidad(cantidadProduccion, materiaPrimaUsada, materiaPrimaRestante) {
-  console.log('Cantidad Producción para predicción:', cantidadProduccion);
-  console.log('Materia Prima Usada para predicción:', materiaPrimaUsada);
-  console.log('Materia Prima Restante para predicción:', materiaPrimaRestante);
+  console.log('Resultado de la predicción:', resultado[0]);
+  return resultado[0];
+};
 
-  const model = await entrenarModelo();
 
-  const input = tf.tensor2d([[parseFloat(cantidadProduccion), parseFloat(materiaPrimaUsada), parseFloat(materiaPrimaRestante)]], [1, 3]);
-  console.log('Input para predicción:', input.toString());
-
-  const prediccion = model.predict(input);
-  console.log('Predicción:', prediccion.toString());
-
-  const resultado = (await prediccion.data())[0];
-  console.log('Resultado de la predicción:', resultado);
-
-  return resultado;
-}
 
 
 
