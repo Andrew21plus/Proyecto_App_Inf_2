dc3852229ad87b908c4ca74706d3e20314da8863
Andrew21plus <andrew21plus@gmail.com>
2024-07-18 21:30:23 -0500

    Se agrega iconos y se mejora los estilos de la pagina de error de servicio

--- Proyecto_App/client/src/pages/ServerErrorComponent.js
+++ Proyecto_App/client/src/pages/ServerErrorComponent.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import '../utils/Server.css';
 
 const ServerErrorComponent = () => {
   return (
     <div className="server-error">
+      <FontAwesomeIcon icon={faExclamationTriangle} size="3x" />
       <h1>Servicio No Disponible</h1>
       <p>Lo sentimos, pero el servidor no está disponible en este momento. Por favor, intente nuevamente más tarde.</p>
     </div>
